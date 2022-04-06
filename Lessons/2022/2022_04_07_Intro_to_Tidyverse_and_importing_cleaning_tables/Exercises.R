a4b7ea75ebc45bddcc0ebc45a3324df032e24ae0
EisenRa <raph.eisenhofer@gmail.com>
2022-04-06 11:25:04 +0930

    adding third lesson!

---
+++ Lessons/2022/2022_04_07_Intro_to_Tidyverse_and_importing_cleaning_tables/Exercises.R
@@ -0,0 +1,44 @@
+### Introduction to the Tidyverse & importing/cleaning tables
+### 
+### Raphael Eisenhofer, 07_04_2022
+
+## 1. Install and then load the "tidyverse" package.
+
+
+## 2. Install and then load the "janitor" package.
+
+
+## 3. Load the "readxl" package (it is bundled within "tidyverse", but not automatically loaded).
+
+
+## 4. Create a vector of length 5, then get the sum of the vector, then get the square root of this. 
+
+
+## 5. Do the same as 4. but with pipes! %>%
+
+
+## 6. Import the "iris_real_world.xlsx" excel sheet into a variable in R
+
+
+## 7. Clean the column headers
+
+
+## 8. Remove empty rows
+
+
+## 9. Remove useless columns
+
+
+## 10. Find any duplicate entries
+
+
+## 11. Remove any duplicate entries by using the distinct() function
+
+
+## 12. BONUS: do steps 6.-> 11. using pipes!!! (note, don't bother with step 10)
+
+
+## 13. Print a summary of your clean(er) table!
+
+
+
