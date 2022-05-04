a57a7b8459d103587e355a8f208b060f8ff990c4
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2022-05-04 10:59:14 +0930

    Add intro to ggplot2 lesson materials

---
+++ Lessons/2022/2022_05_05_Intro_to_ggplot2/Exercise.R
@@ -0,0 +1,43 @@
+##  INTRO TO GGPLOT2 EXERCISES  ##
+
+## Shortcut Reminder:
+  # ctrl + enter (or cmnd + enter) to run the line of code your cursor is currently on
+  # 'tab' to autocomplete, arrow up and down through the options
+
+## Setting up your workspace
+  
+  ## load libraries (By loading 'tidyverse', a number of tidyverse packages will be loaded including ggplot2 (listed in console). 
+                   # However, you will need to load janitor and readxl separately.)
+
+## Import our dataset and clean it up! 
+  #(Reminder: you need to fix column names, empty rows, redundant columns and rows, NAs, species values, and the data type of variables)#
+
+
+
+
+## Now that our data is clean, lets get plotting!!
+
+## Create a scatter plot of sepal_length x sepal_width
+
+
+  ## colour points by species
+
+
+  ## separate plot into facets
+
+
+  ## add in linear trend line
+
+
+  ## clean up labels
+
+
+  ## remove redundant labels
+
+
+## Create a plot showing the distribution of petal width for each species, remember to tidy your axes.
+
+
+
+
+## Practise saving your plots
