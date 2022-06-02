231199544fc1ff20fc02fec87b659b1cf6297cc4
EisenRa <raph.eisenhofer@gmail.com>
2022-06-02 12:29:13 +1200

    adding lesson

---
+++ Lessons/2022/2022_06_02_Joining_pivoting_tables/Exercises.R
@@ -0,0 +1,25 @@
+## You are collaborating with the international institute of dog science (IIDS) on
+## a research project. There are many people working on the project, and each has
+## their own way of collecting data *facepalm*. In these exercises we will cover
+## this real world scenario using some dummy data, with the goal of having a 
+## final table containing all metadata for the dogs.
+
+## Load your required packages
+
+## 1: Import the four data files.
+
+
+## 2: Let's start by getting the 'dog_food_walk_data.txt' tidied up.
+
+
+## 3: Now, let's also tidy up the 'dog_bday_data.txt' file.
+
+
+## 4: Next up, the 'dog_treats_data.csv' file. 
+
+
+## 5: Finally, let's tidy the 'dog_weights.csv' file.
+
+
+## 6: Now we want to combine all these tables into a single table:
+
