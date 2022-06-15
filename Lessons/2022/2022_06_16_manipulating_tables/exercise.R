dcd9d9bf9e75dc5e03690cf89d79b682f77f46ee
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2022-06-16 00:25:55 +0930

    Upload lesson for 2022_06_16

---
+++ Lessons/2022/2022_06_16_manipulating_tables/exercise.R
@@ -0,0 +1,44 @@
+##  EXERCISES: MANIPULATING TABLES 16/06/22   ##
+
+## Shortcut Reminder:
+# ctrl + enter (or cmnd + enter) to run the line of code your cursor is currently on
+# 'tab' to autocomplete, arrow up and down through the options
+
+## load packages ##
+
+
+
+## load data ##
+
+
+
+## reformat test results so they are comparable to the online test result data
+
+
+##check if the two data frames are the same
+
+
+## compare the datasets to see what rows are shared and what are not.
+
+
+
+##what do all the rows frorm both tables look like together
+
+
+
+## looks like theres an error in one of the tables, assume the online results are correct for alfie's data 
+##use the join functions to decide on the best join for the data so you will have the test scores for all 6 students
+
+
+
+## join the two datasets 
+
+
+
+##extract the data for test 1 to calculate the mean score
+
+
+
+##extract test 4 for as a table with the students names 
+
+
