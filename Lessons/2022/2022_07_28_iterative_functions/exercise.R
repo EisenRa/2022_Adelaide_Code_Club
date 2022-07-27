ddeaea7da6bd81572f5f7c248c55ac20ea0631ad
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2022-07-27 17:14:08 +0930

    Content for iterative functions

---
+++ Lessons/2022/2022_07_28_iterative_functions/exercise.R
@@ -0,0 +1,40 @@
+## Iterative functions ##
+
+## Shortcut Reminder:
+# ctrl + enter (or cmnd + enter) to run the line of code your cursor is currently on
+# 'tab' to autocomplete, arrow up and down through the options
+
+## for loops
+  # create a vector of values 1 to 10
+
+
+  # create for loop to print out each value
+
+
+  # create a for loop to only print out values divisible by 2
+
+
+  # create another vector of values ranging 1 to 5
+
+
+  # create a for loop that multiplies values from the two vectors and prints those divisible by 3
+
+
+
+## apply functions
+
+df <- data.frame(x = 1:4, y = 25:38, z=89:92)
+
+
+  # create a vector of the mean of each row of df
+
+
+  # apply the function sqrt() to all values in the vector
+
+
+  # create code that will print "The square root of [value] is [square root of value]." 
+  # using the values obtained using the apply functions
+
+
+
+
