993b5e268e863e12300c9ca7371fbe2d394b4962
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2022-08-10 17:27:39 +0930

    data.table lesson content

---
+++ Lessons/2022/2022_08_11_data.table/exercise.R
@@ -0,0 +1,34 @@
+## data.table ##
+
+## Shortcut Reminder:
+# ctrl + enter (or cmnd + enter) to run the line of code your cursor is currently on
+# 'tab' to autocomplete, arrow up and down through the options
+
+## install and load data.table
+
+
+## read in iris data
+
+
+## subset the first 10 rows
+
+
+## subset all rows where Petal.Width is equal to 1.8
+
+
+## extract just the Species and Petal.Width columns
+
+
+## ectract the Sepal.Width column as a vector
+
+
+## count the number of rows for each species
+
+
+## calculate the mean sepal length per species
+
+
+## BONUS QUESTION: Plot petal length vs petal width for only setosa species, using data.table syntax to subset the data
+
+
+
