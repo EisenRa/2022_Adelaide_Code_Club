efee5fbc9faa8a4b8338f78d8e0eca9dea5280a8
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2022-08-23 22:06:01 +0930

    Upload new lesson

---
+++ Lessons/2022/2022_08_25_manipulating_data.tables/exercise.R
@@ -0,0 +1,39 @@
+## manipulating data.tables ##
+
+## Shortcut Reminder:
+# ctrl + enter (or cmnd + enter) to run the line of code your cursor is currently on
+# 'tab' to autocomplete, arrow up and down through the options
+
+## load data.table
+
+
+## read in air quality data
+
+
+## subset the data from the month of June
+
+
+## calculate the mean of the Temp
+
+
+## subset all rows where temp is over the mean
+
+
+## add a column to the DT containing the temp in degrees celcius. F to C = (temp in F − 32) × 5/9
+
+
+## add two more columns, 1. an identifier of both the date and time and 2. the temperature in K (C to K = temp in C + 273.15)
+
+
+## check that Temp is the dame data type as the other temperature values and change it if it isn't
+
+
+## reassign column name to make the temp in F clear
+
+
+## remove the Ozone, Solar.R and Wind columns 
+
+
+## extract the mean, median, min, and max of the temperature in C
+
+
