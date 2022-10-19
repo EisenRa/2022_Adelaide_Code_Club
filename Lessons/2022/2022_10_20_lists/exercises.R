32ceb00124d8f5eb98eccbc3233c0a87e0a21484
EisenRa <raph.eisenhofer@gmail.com>
2022-10-19 20:44:06 +0200

    add list lesson files

---
+++ Lessons/2022/2022_10_20_lists/exercises.R
@@ -0,0 +1,17 @@
+# 1) Explore the sw_films list. How many lists are in there? How many lists are within each list? str(sw_films)
+
+
+# 2) Interactively view the list using listviewer::jsonedit(got_chars, mode = "view")
+
+
+# 3) View the titles of each star wars film 
+
+
+# 4) Extract the titles into a character vector
+
+
+# 5) View both the title and year 
+
+
+# 6) Create a dataframe with the title, episde number, who directed it, the release date, and the characters in it
+
