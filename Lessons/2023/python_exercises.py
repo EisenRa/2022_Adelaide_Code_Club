594510949d8bbf42637cf84cbaedcd2500826701
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2023-02-01 16:17:57 +1030

    Upload python exercise

---
+++ Lessons/2023/python_exercises.py
@@ -0,0 +1,37 @@
+### Intro to python 02/02/2023  ###
+
+## create a variable equal to 5
+
+
+## reassign the variable to be five times the original value
+
+
+## create a list/array of the number 1 through 5
+
+
+## write a for loop using your list where each value is reassined to be half its original value
+
+
+## print the new values for your list
+
+
+## extract the first value in the list
+
+
+## create a new list that consists of every second value from your first list
+
+
+## print the list in reverse
+
+
+## add -0.5 to the end of your list
+
+
+## import numpy
+
+
+## convert your list to a numpy array
+
+
+## what are the maximum and minimun values of your array
+
